834b7b43bf5f62b54e0027ecaa7895b3e247c1ad
Raghunath Venkata <sv_raghunath@hotmail.com>
2019-10-05 21:14:39 +0100

    changes to lorem epsom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const home = () => {
       <Hero>
         <Banner
           title="luxurious rooms"
-          subtitle="deluxe rooms starting at $299"
+          subtitle="deluxe rooms starting at ₹299"
         >
           <Link to="/rooms" className="btn-primary">
             our rooms
